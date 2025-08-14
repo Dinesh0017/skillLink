1dc7ca3f4e1e3effa3f30beb771d8f5ba1c9b807
Dinesh Rathnasiri <dineshrathnasiri017@gmail.com>
2025-08-14 20:01:24 +0530

    add new component
    TopExperts.jsx added

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Navbar from '../components/Navbar'
 import Hero from '../components/Hero'
 import PopularServices from '../components/PopularServices'
+import TopExperts from '../components/TopExperts'
 
 
 const HomePage = () => {
@@ -10,6 +11,7 @@ const HomePage = () => {
     <Navbar />
     <Hero />
     <PopularServices />
+    <TopExperts />
     </>
   )
 }
